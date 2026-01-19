dd623d7a10a684e33eedc29cb0a04785bb12bb73
Renaolol <renanschmeier91@gmail.com>
2026-01-19 17:01:18 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,3 +1,12 @@
 import streamlit as st
 st.set_page_config(layout="wide")
+
 st.title("BI Gcont gestão")
+st.divider()
+st.header("Bem Vindo(a)")
+st.write("Para acessar as diferentes abas do BI, utilize o menu lateral")
+st.write("Ou clique em um dos links abaixo")
+st.link_button("Administrar","Administrar")
+st.link_button("AliqSimples","AliqSimples")
+st.link_button("Contabil","Contabil")
+st.link_button("Empresas","Empresas")
